79489f0951afe9739808b5eb0af724189ed9d97a
Bhavya Panchal <panchalbhavya2210@gmail.com>
2025-04-21 23:53:48 +0530

    ant

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -6,7 +6,10 @@ function About() {
       <Helmet>
         <title>About | Try-FF</title>
         <meta property="og:title" content="About Try-FF" />
-        <meta property="og:description" content="Learn more about Try-FF." />
+        <meta
+          property="og:description"
+          content="Learn more about Try-FF.fffff"
+        />
         <meta
           property="og:image"
           content="https://try-ff.vercel.app/og-about.jpg"
